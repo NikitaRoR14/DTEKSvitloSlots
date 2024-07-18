632dd48661e583b4798846cf88249934485dc45c
NikitaROR <nporoshin14@icloud.com>
2024-07-18 22:09:19 +0300

    Add final improvements

--- IOS-DTEKSlotMachine/Controller/ViewController.swift
+++ IOS-DTEKSlotMachine/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBOutlet weak var pickerView: UIPickerView!
@@ -19,14 +19,61 @@ class ViewController: UIViewController {
         resultLabel.text = ""
         pickerView.dataSource = self
         pickerView.delegate = self
-    }
-
-    @IBAction func buttonPressed(_ sender: UIButton) {
+        pickerView.isUserInteractionEnabled = false
         
     }
     
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.model.playSound(soundName: K.buttonPressedSound, fileType: "mp3" )
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.40, execute: {
+            self.roll()
+        })
+        
+    }
     
+    func roll(){
+        var delay : Double = 0
+        
+        for i in 0..<pickerView.numberOfComponents{
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                self.model.playSound(soundName: K.spinSound, fileType: "mp3")
+                self.randomSelectRow(in: i)
+            })
+            delay += 0.30
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.checkResult()
+        }
+       
+    }
+    
+    
+    func randomSelectRow(in comp : Int){
+        let r = Int(arc4random_uniform(UInt32(8 * Model.images.count))) + Model.images.count
+        pickerView.selectRow(r, inComponent: comp, animated: true)
+        
+    }
+    func checkResult(){
+        var counter = 0.0
+        for i in 0..<pickerView.numberOfComponents{
+            let row : Int = pickerView.selectedRow(inComponent: i) % Model.images.count
+            counter += model.countLight(row)
+        }
+        resultLabel.text = model.sumAllPoints(counter).0
+        let soundName = model.sumAllPoints(counter).1
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.10, execute: {
+            self.model.playSound(soundName: soundName, fileType: "wav")
+        })
+        
+    }
 }
+
+
+
+
+
 //MARK: - UIPickerViewDataSource
 extension ViewController:UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -34,17 +81,21 @@ extension ViewController:UIPickerViewDataSource{
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return model.images.count * 10
+        return Model.images.count * 10
     }
+    
+    
 }
 //MARK: - UIPickerViewDelegate
 extension ViewController:UIPickerViewDelegate{
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        return UIImageView(image: model.images[row % model.images.count])
+        return UIImageView(image: Model.images[row % Model.images.count])
     }
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return model.images[component % model.images.count].size.height + 1
+        return Model.images[component % Model.images.count].size.height + 3
     }
+    
 }
+
 
 
--- IOS-DTEKSlotMachine/Model/Model.swift
+++ IOS-DTEKSlotMachine/Model/Model.swift
@@ -7,18 +7,61 @@
 
 import Foundation
 import UIKit
-
+import AVFoundation
 
 struct Model{
-    let images = [#imageLiteral(resourceName: "PossibleLight") , #imageLiteral(resourceName: "LightOn"), #imageLiteral(resourceName: "NoLight")]
+    static let images = [#imageLiteral(resourceName: "LightOn"),  #imageLiteral(resourceName: "PossibleLight") ,  #imageLiteral(resourceName: "NoLight")]
     
-    func countLight(_ row:Int) ->Int {
-        let image = images[row]
-        switch image.description{
+    var audioPlayer = AVAudioPlayer()
+    
+    
+    mutating func playSound(soundName:String,fileType:String) {
+        guard let path = Bundle.main.path(forResource: "\(soundName)", ofType:"\(fileType)") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
             
-        default:
-            //
+        } catch let error {
+            print(error.localizedDescription)
         }
+    }
+    
+    func countLight(_ row:Int) ->Double {
+        let image = Model.images[row]
+        var result:Double = 0.0
+        switch image.description{
+        case K.lightOnImage:
+            result = 1.0
+        case K.noLightImage:
+            result = 0.0
+        
+        case K.possibleLightImage:
+            result = 0.5
+        default:
+            print("error img desc")
+        }
+        return result
+    }
+     func sumAllPoints(_ counter:Double) -> (String,String){
+        var resultMessage = ""
+        var soundName = ""
+        switch counter{
+        case 0.0..<1.5:
+            resultMessage = K.noElectricity.randomElement()!
+            soundName = K.noLightSound
+        case 1.5:
+            resultMessage = K.electricityPossible.randomElement()!
+            soundName = K.possibleLightSound
+        case 1.6...:
+            resultMessage = K.electricityOn.randomElement()!
+            soundName = K.lightOnSound
+        default:
+            print("error result message")
+        }
+        return (resultMessage,soundName)
     }
 }
 
